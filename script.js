cbeff8d14402efc0a1d6dcc78887033ec68bbdda
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-14 18:50:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function fancyCal() {
+	const firstIn = parseInt(prompt("Enter first number"));
+	const firstMul = firstIn*2;
+
+	const secondIn = parseInt(prompt("Enter second number"));
+	const addNum = firstMul + secondIn;
+	return addNum; 
+}
+let result = fancyCal();
+alert(`The final result is: ${result}`);
